f764c559ce1d9451686101856f01ec7060bfafa2
remi taunay <remi.taunay@gmail.com>
2022-12-08 23:07:35 +0100

    fix: fr for macro

--- keyboards/ferris/keymaps/reezi/keymap.c
+++ keyboards/ferris/keymaps/reezi/keymap.c
@@ -1,7 +1,9 @@
 #include QMK_KEYBOARD_H
 
 // https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/keymap_french.h
+// https://docs.qmk.fm/#/feature_macros?id=alternative-keymaps
 #include "keymap_french.h"
+#include "sendstring_colemak.h"
 
 // https://getreuer.info/posts/keyboards/custom-shift-keys/index.html
 #include "features/custom_shift_keys.h"
