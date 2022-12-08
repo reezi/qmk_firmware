b74bd8c829020eae1fa9aa0ee97ffc984e6d3341
remi taunay <remi.taunay@gmail.com>
2022-12-08 23:51:55 +0100

    config.h

---
+++ keyboards/ferris/keymaps/reezi/config.h
@@ -0,0 +1,8 @@
+// https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/keymap_french.h
+#include "keymap_french.h"
+
+// https://docs.qmk.fm/#/feature_macros?id=alternative-keymaps
+#include "sendstring_colemak.h"
+
+// https://getreuer.info/posts/keyboards/custom-shift-keys/index.html
+#include "features/custom_shift_keys.h"
--- keyboards/ferris/keymaps/reezi/keymap.c
+++ keyboards/ferris/keymaps/reezi/keymap.c
@@ -1,12 +1,5 @@
 #include QMK_KEYBOARD_H
-
-// https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/keymap_french.h
-// https://docs.qmk.fm/#/feature_macros?id=alternative-keymaps
-#include "keymap_french.h"
-#include "sendstring_colemak.h"
-
-// https://getreuer.info/posts/keyboards/custom-shift-keys/index.html
-#include "features/custom_shift_keys.h"
+#include "config.h"
 
 // custom keycodes for qmk macro
 enum custom_keycodes {
