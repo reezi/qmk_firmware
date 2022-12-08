64649c0d2f5b309b993947df3bc1e5b7e1efd2a6
remi taunay <remi.taunay@gmail.com>
2022-12-08 23:55:45 +0100

    fix: fr

--- keyboards/ferris/keymaps/reezi/config.h
+++ keyboards/ferris/keymaps/reezi/config.h
@@ -1,8 +1,10 @@
-// https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/keymap_french.h
-#include "keymap_french.h"
+// https://getreuer.info/posts/keyboards/custom-shift-keys/index.html
+#include "features/custom_shift_keys.h"
 
+// https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/keymap_french.h
 // https://docs.qmk.fm/#/feature_macros?id=alternative-keymaps
-#include "sendstring_colemak.h"
+#include "keymap_french.h"
+#include "sendstring_french.h"
 
 // https://getreuer.info/posts/keyboards/custom-shift-keys/index.html
 #include "features/custom_shift_keys.h"
