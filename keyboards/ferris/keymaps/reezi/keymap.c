cb433bf2399ff0dedaeda8c17c319d862dc10a93
remi taunay <remi.taunay@gmail.com>
2022-12-17 18:06:38 +0100

    combo: doube and non-dead symbol on home row

--- keyboards/ferris/keymaps/reezi/keymap.c
+++ keyboards/ferris/keymaps/reezi/keymap.c
@@ -2,7 +2,6 @@
 #include QMK_KEYBOARD_H
 #include "sendstring_french.h"
 #include "features/custom_shift_keys.h" // https://getreuer.info/posts/keyboards/custom-shift-keys/index.html
-#include "g/keymap_combo.h" 
 
 enum custom_keycodes {
     C_FLASH = SAFE_RANGE,
@@ -10,11 +9,13 @@ enum custom_keycodes {
     C_SQUO, C_DQUO, C_BQUO,
     C_ANBR, C_CYBR, C_NMBR, C_PARE,
 };
-
 #define _ABC 0
 #define _EEE 1
 #define _SYS 2
 #define _SYM 3
+
+#include "g/keymap_combo.h" 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_ABC] = LAYOUT_split_3x5_2(
       FR_B, FR_Y, FR_O, FR_U, _______, _______, FR_L, FR_D, FR_W, FR_V,
@@ -29,14 +30,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TO(_ABC), _______, _______, _______
   ),
 	[_SYS] = LAYOUT_split_3x5_2(
-      _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______, LT(0,C_FLASH),
+      _______, _______, _______, _______, _______, _______, KC_PSCR, _______, QK_BOOTLOADER, LT(0,C_FLASH),
       KC_BRID, KC_VOLD, KC_PGDN, KC_END, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
       _______, OSM(MOD_LALT), KC_ESC, KC_TAB, _______, _______, KC_BSPC, KC_ENTER, KC_DEL, _______,
       TO(_ABC), OSM(MOD_LCTL), OSM(MOD_LSFT), TO(_SYM)
   ),
 	[_SYM] = LAYOUT_split_3x5_2(
-      FR_AT, LT(0,C_PARE), LT(0,C_CYBR), LT(0,C_NMBR), FR_EXLM, FR_PERC, FR_SLSH, FR_DLR, FR_PIPE, FR_EQL,
-      LT(0,C_ANBR), LT(0,C_TILD), LT(0,C_SQUO), LT(0,C_DQUO), LT(0,C_BQUO), FR_HASH, FR_COLN, FR_MINS, FR_DOT, FR_ASTR,
+      FR_AT, _______, _______, _______, FR_EXLM, FR_PERC, FR_SLSH, FR_DLR, FR_PIPE, FR_EQL,
+      _______, _______, _______, _______, _______, FR_HASH, FR_COLN, FR_MINS, FR_DOT, FR_ASTR,
       KC_P1, KC_P2, KC_P3, KC_P4, KC_P5, KC_P6, KC_P7, KC_P8, KC_P9, KC_P0,
       TO(_ABC), _______, OSM(MOD_LSFT), TO(_SYS)
   )
--- keyboards/ferris/keymaps/reezi/logic.c
+++ keyboards/ferris/keymaps/reezi/logic.c
@@ -6,39 +6,39 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_custom_shift_keys(keycode, record)) { return false; }
     switch (keycode) {
       case LT(0,C_FLASH):
-        if (TAP) { SS("qmk flash" SS_TAP(X_ENTER)); return false; }
-        else if (HOLD) { tap_code16(QK_BOOTLOADER); return false; }
-        break;
+        if (TAP) { SS("qmk flash" SS_TAP(X_ENTER)); }
+        else if (HOLD) { tap_code16(QK_BOOTLOADER); }
+        return false;
       case LT(0,C_ANBR):
-        if (TAP) { SS("<"); return false; }
-        else if (HOLD) { SS("<>" SS_TAP(X_LEFT)); return false; }
-        break;
+        if (TAP) { SS("<"); }
+        else if (HOLD) { SS("<>" SS_TAP(X_LEFT)); }
+        return false;
       case LT(0,C_CYBR):
-        if (TAP) { SS("{"); return false; }
-        else if (HOLD) { SS("{}" SS_TAP(X_LEFT)); return false; }
-        break;
+        if (TAP) { SS("{"); }
+        else if (HOLD) { SS("{}" SS_TAP(X_LEFT)); }
+        return false;
       case LT(0,C_NMBR):
-        if (TAP) { SS("["); return false; }
-        else if (HOLD) { SS("[]" SS_TAP(X_LEFT)); return false; }
-        break;
+        if (TAP) { SS("["); }
+        else if (HOLD) { SS("[]" SS_TAP(X_LEFT)); }
+        return false;
       case LT(0,C_PARE):
-        if (TAP) { SS("("); return false; }
-        else if (HOLD) { SS("()" SS_TAP(X_LEFT)); return false; }
-        break;
+        if (TAP) { SS("("); }
+        else if (HOLD) { SS("()" SS_TAP(X_LEFT)); }
+        return false;
       case LT(0,C_TILD): // dead
-        if (TAP) { tap_code16(FR_TILD); return false; }
-        else if (HOLD) { tap_code16(FR_TILD); tap_code16(FR_SLSH); return false; }
-        break;
+        if (TAP) { tap_code16(FR_TILD); }
+        else if (HOLD) { tap_code16(FR_TILD); tap_code16(FR_SLSH); }
+        return false;
       case LT(0,C_SQUO):
-        if (TAP) { SS("'"); return false; }
-        else if (HOLD) { SS("''" SS_TAP(X_LEFT)); return false; }
-        break;
+        if (TAP) { SS("'"); }
+        else if (HOLD) { SS("''" SS_TAP(X_LEFT)); }
+        return false;
       case LT(0,C_DQUO):
-        if (TAP) { SS("\""); return false; }
-        else if (HOLD) { SS("\"\"" SS_TAP(X_LEFT)); return false; }
-        break;
+        if (TAP) { SS("\""); }
+        else if (HOLD) { SS("\"\"" SS_TAP(X_LEFT)); }
+        return false;
       case LT(0,C_BQUO): // dead
-        if (TAP) { tap_code16(FR_GRV); return false; }
+        if (TAP) { tap_code16(FR_GRV); }
         else if (HOLD) {
           tap_code16(FR_GRV);
           tap_code16(FR_GRV);
@@ -49,19 +49,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           tap_code16(FR_GRV);
           tap_code16(KC_UP);
           tap_code16(KC_END);
-          return false;
         }
-        break;
+        return false;
       case C_CIRC: // dead
-        if (TAP) { tap_code16(FR_CIRC); return false; }
-        else if (HOLD) { tap_code16(FR_CIRC); tap_code16(KC_SPC); return false; }
-        break;
-      default:
-        if ((KC_A <= keycode) && (keycode <= KC_Z)) { // alpha
-          if (TAP) { tap_code16(keycode); return false; }
-          else if (HOLD) { tap_code16(LSFT(keycode)); return false; }
-        }
-        break;
+        if (TAP) { tap_code16(FR_CIRC); }
+        else if (HOLD) { tap_code16(FR_CIRC); tap_code16(KC_SPC); }
+        return false;
+//      default:
+//        if ((FR_A <= keycode) && (keycode <= FR_Z)) { // alpha
+//          if (TAP) { tap_code16(keycode); }
+//          else if (HOLD) { tap_code16(LSFT(keycode)); }
+//        }
+//        return false;
     }
     return true;
 };
