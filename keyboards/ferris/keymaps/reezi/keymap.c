ea6fab6269b75d64c9f2e02d58f555063860e0a8
remi taunay <remi.taunay@gmail.com>
2022-12-17 22:52:00 +0100

    layout: use center columns on _ABC

--- keyboards/ferris/keymaps/reezi/keymap.c
+++ keyboards/ferris/keymaps/reezi/keymap.c
@@ -9,6 +9,9 @@ enum custom_keycodes {
     C_SQUO, C_DQUO, C_BQUO,
     C_ANBR, C_CYBR, C_NMBR, C_PARE,
 };
+#define OS_LSFT OSM(MOD_LSFT)
+#define OS_LALT OSM(MOD_LALT)
+#define OS_LCTL OSM(MOD_LCTL)
 #define _ABC 0
 #define _EEE 1
 #define _SYS 2
@@ -18,28 +21,28 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_ABC] = LAYOUT_split_3x5_2(
-      FR_B, FR_Y, FR_O, FR_U, _______, _______, FR_L, FR_D, FR_W, FR_V,
-      FR_C, FR_I, FR_E, FR_A, _______, _______, FR_H, FR_T, FR_S, FR_N,
-      FR_G, FR_X, FR_J, FR_K, _______, _______, FR_R, FR_M, FR_F, FR_P,
-      MEH_T(KC_SPACE), _______, OSM(MOD_LSFT), TO(_SYM)
+      FR_B, FR_Y, FR_O, FR_U, FR_MINS, FR_QUES, FR_L, FR_D, FR_W, FR_V,
+      FR_C, FR_I, FR_E, FR_A, FR_COMM, FR_DOT,  FR_H, FR_T, FR_S, FR_N,
+      FR_G, FR_X, FR_J, FR_K, FR_Z,    FR_Q,    FR_R, FR_M, FR_F, FR_P,
+      MEH_T(KC_SPACE), _______, OS_LSFT, TO(_SYM)
   ),
 	[_EEE] = LAYOUT_split_3x5_2(
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-      FR_CCED, FR_EGRV, FR_EACU, FR_AGRV, _______, _______, FR_DIAE, C_CIRC, _______, _______,
+      FR_CCED, FR_EGRV, FR_EACU, FR_AGRV, _______, _______, FR_DIAE,  C_CIRC, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       TO(_ABC), _______, _______, _______
   ),
 	[_SYS] = LAYOUT_split_3x5_2(
       _______, _______, _______, _______, _______, _______, KC_PSCR, _______, LT(0,C_FLASH), QK_BOOTLOADER,
-      KC_BRID, KC_VOLD, KC_PGDN, KC_END, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
-      _______, OSM(MOD_LALT), KC_ESC, KC_TAB, _______, _______, KC_BSPC, KC_ENTER, KC_DEL, _______,
-      TO(_ABC), OSM(MOD_LCTL), OSM(MOD_LSFT), TO(_SYM)
+      KC_BRID, KC_VOLD, KC_PGDN, KC_END,  _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
+      _______, OS_LALT, KC_ESC,  KC_TAB,  _______, _______, KC_BSPC, KC_ENTER, KC_DEL, _______,
+      TO(_ABC), OS_LCTL, OS_LSFT, TO(_SYM)
   ),
 	[_SYM] = LAYOUT_split_3x5_2(
-      FR_AT, _______, _______, _______, FR_EXLM, FR_PERC, FR_SLSH, FR_DLR, FR_PIPE, FR_EQL,
-      _______, _______, _______, _______, _______, FR_HASH, FR_COLN, FR_MINS, FR_DOT, FR_ASTR,
-      KC_P1, KC_P2, KC_P3, KC_P4, KC_P5, KC_P6, KC_P7, KC_P8, KC_P9, KC_P0,
-      TO(_ABC), _______, OSM(MOD_LSFT), TO(_SYS)
+      _______, FR_PERC, FR_ASTR, FR_PLUS, FR_MINS, FR_QUES, FR_AMPR, FR_DLR, FR_BSLS, C_BQUO,
+      _______, FR_AT,   FR_HASH, C_ANBR,  FR_COMM, FR_DOT,  FR_PIPE, FR_EQL, FR_SLSH, _______,
+      KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5, KC_P6,     KC_P7,   KC_P8,  KC_P9,   KC_P0,
+      TO(_ABC), _______, OS_LSFT, TO(_SYS)
   )
 };
 
--- keyboards/ferris/keymaps/reezi/shift.c
+++ keyboards/ferris/keymaps/reezi/shift.c
@@ -1,16 +1,13 @@
 const custom_shift_key_t custom_shift_keys[] = {
-  {FR_EXLM, FR_QUES}, // shift ? is !
-  {FR_SLSH, FR_BSLS}, // shift / is "\"
-  {FR_DLR, FR_EURO}, // shift $ is €
-  {FR_EQL, FR_AMPR}, // shift = is &
-  {LT(0,C_ANBR), FR_RABK}, // shift < is >
-  {FR_COLN, FR_SCLN}, // shift : is ;
   {FR_MINS, FR_UNDS}, // shift - is _
-  {FR_DOT, FR_COMM}, // shift . is ,
-  {FR_ASTR, FR_PLUS}, // shift * is +
+  {FR_QUES, FR_EXLM}, // shift ? is !
+  {FR_COMM, FR_SCLN}, // shift , is ;
+  {FR_DOT, FR_COLN}, // shift . is :
+  {LT(0,C_ANBR), FR_RABK}, // shift < is >
   {LT(0,C_CYBR), FR_RCBR}, // shift { is }
   {LT(0,C_NMBR), FR_RBRC}, // shift [ is ]
   {LT(0,C_PARE), FR_RPRN}, // shift ( is )
+  {FR_DLR, FR_EURO}, // shift $ is €
   {KC_BRID, KC_BRIU}, // shift bri-- is bri++
   {KC_VOLD, KC_VOLU}, // shift vol-- is vol++
   {KC_PGDN, KC_PGUP}, // shift pagedown is pageup
