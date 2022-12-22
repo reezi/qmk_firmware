ca205538ed593e1101c54a7dc0af56df6c27d678
remi taunay <remi.taunay@gmail.com>
2022-12-22 20:12:52 +0100

    chore: indent

--- keyboards/ferris/keymaps/reezi/keymap.c
+++ keyboards/ferris/keymaps/reezi/keymap.c
@@ -24,8 +24,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_ABC] = LAYOUT_split_3x5_2(
       FR_B, FR_Y, FR_O, LT(0,C_U), FR_MINS, FR_UNDS, FR_L, FR_D, FR_W, FR_V,
-      FR_C, FR_I, FR_E, FR_A, FR_COMM, FR_DOT,  FR_H, FR_T, FR_S, FR_N,
-      FR_G, FR_X, FR_J, FR_K, FR_Z,    FR_Q,    FR_R, FR_M, FR_F, FR_P,
+      FR_C, FR_I, FR_E, FR_A,      FR_COMM, FR_DOT,  FR_H, FR_T, FR_S, FR_N,
+      FR_G, FR_X, FR_J, FR_K,      FR_Z,    FR_Q,    FR_R, FR_M, FR_F, FR_P,
       MEH_T(KC_SPACE), _______, OS_LSFT, TO(_SYM)
   ),
 	[_EEE] = LAYOUT_split_3x5_2(
