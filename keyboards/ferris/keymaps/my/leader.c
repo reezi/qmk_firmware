18b2d5b75d46191e28fdd8392c9c81714f1d84a7
reezi <reezi@gmail.com>
2023-01-27 18:07:28 +0100

    leader: app switching

--- keyboards/ferris/keymaps/my/leader.c
+++ keyboards/ferris/keymaps/my/leader.c
@@ -5,22 +5,19 @@ void matrix_scan_user(void) {
     leading = false;
     leader_end();
 
-    // wow layer
-    SEQ_THREE_KEYS(FR_W, FR_O, FR_W) {
-      layer_move(_WST);
-    }
+    SEQ_ONE_KEY(FR_W) { SS(SS_C(SS_S(SS_A(SS_LGUI("w"))))); } // web
+    SEQ_ONE_KEY(FR_C) { SS(SS_C(SS_S(SS_A(SS_LGUI("c"))))); } // chat
+    SEQ_ONE_KEY(FR_E) { SS(SS_C(SS_S(SS_A(SS_LGUI("e"))))); } // editor
+    SEQ_ONE_KEY(FR_T) { SS(SS_C(SS_S(SS_A(SS_LGUI("t"))))); } // terminal
+    SEQ_ONE_KEY(FR_G) { SS(SS_C(SS_S(SS_A(SS_LGUI("g"))))); } // game
 
-    // web
-    SEQ_ONE_KEY(FR_W) {
-      SS(SS_C(SS_S(SS_A(SS_LGUI("w")))));
-    }
-
-    // web search
     SEQ_TWO_KEYS(FR_W, FR_W) {
       SS(SS_C(SS_S(SS_A(SS_LGUI("w")))));
       SS(SS_C("t"));
       SS(SS_A("d"));
-    }
+    } // web search
+
+    SEQ_THREE_KEYS(FR_W, FR_O, FR_W) { layer_move(_WST); } // wow layer
 
   }
 }
