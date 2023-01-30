af3a6510e2ca1e9687eadf07ec3350249f8816d2
reezi <reezi@gmail.com>
2023-01-30 19:28:12 +0100

    leader: namespace sway shorthands under gui+s

--- keyboards/ferris/keymaps/my/leader.c
+++ keyboards/ferris/keymaps/my/leader.c
@@ -5,17 +5,14 @@ void matrix_scan_user(void) {
     leading = false;
     leader_end();
 
-    SEQ_ONE_KEY(FR_W) { SS(SS_HYPER("w")); } // web
-    SEQ_ONE_KEY(FR_C) { SS(SS_HYPER("c")); } // chat
-    SEQ_ONE_KEY(FR_E) { SS(SS_HYPER("e")); } // editor
-    SEQ_ONE_KEY(FR_T) { SS(SS_HYPER("t")); } // terminal
-    SEQ_ONE_KEY(FR_G) { SS(SS_HYPER("g")); } // game
+    SEQ_ONE_KEY(FR_S) { set_oneshot_mods(MOD_BIT(KC_LGUI)); } // lgui is my sway mod key
+    SEQ_ONE_KEY(FR_T) { SS(SS_G("st")); } // terminal
+    SEQ_ONE_KEY(FR_E) { SS(SS_G("se")); } // editor
+    SEQ_ONE_KEY(FR_W) { SS(SS_G("sw")); } // web
+    SEQ_ONE_KEY(FR_C) { SS(SS_G("sc")); } // chat
+    SEQ_ONE_KEY(FR_G) { SS(SS_G("sg")); } // game
 
-    SEQ_TWO_KEYS(FR_W, FR_W) {
-      SS(SS_HYPER("w"));
-      SS(SS_C("t"));
-      SS(SS_A("d"));
-    } // web search
+    SEQ_TWO_KEYS(FR_W, FR_S) { SS(SS_HYPER("w")); SS(SS_C("t")); SS(SS_A("d")); } // web search
 
     SEQ_THREE_KEYS(FR_W, FR_O, FR_W) { layer_move(_WST); } // wow layer
 
