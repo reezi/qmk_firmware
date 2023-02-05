99ab3f3d5204e60c119d6964ddd667338809e2b5
reezi <reezi@gmail.com>
2023-02-05 19:54:44 +0100

    leader: easier tty

--- keyboards/ferris/keymaps/my/leader.c
+++ keyboards/ferris/keymaps/my/leader.c
@@ -12,15 +12,15 @@ void matrix_scan_user(void) {
     SEQ_ONE_KEY(FR_C) { SS(SS_CG("c")); } // chat
 
     SEQ_TWO_KEYS(FR_W, FR_S) { SS(SS_G("ws")); SS(SS_C("t")); SS(SS_A("d")); } // web search
+    SEQ_TWO_KEYS(FR_T, FR_H) { SS(SS_CA(SS_TAP(X_F1))); } // tty 1
+    SEQ_TWO_KEYS(FR_T, FR_S) { SS(SS_CA(SS_TAP(X_F2))); } // tty 2
 
     SEQ_THREE_KEYS(FR_W, FR_O, FR_W) { layer_move(_WST); } // wow layer
     SEQ_THREE_KEYS(FR_S, FR_S, FR_R) { SS(SS_CG( SS_DOWN(X_S) SS_DOWN(X_R) SS_UP(X_R) SS_UP(X_S) )); } // sway reload
     SEQ_THREE_KEYS(FR_S, FR_S, FR_K) { SS(SS_CG( SS_DOWN(X_S) SS_DOWN(X_K) SS_UP(X_K) SS_UP(X_S) )); } // sway kill
-    SEQ_THREE_KEYS(FR_T, FR_T, FR_Y) { SS(SS_CA(SS_TAP(X_F1))); } // tty 1
 
     SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_1) { SS(SS_CA(SS_TAP(X_F1))); } // tty 1
     SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_2) { SS(SS_CA(SS_TAP(X_F2))); } // tty 2
-    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_Y) { SS(SS_CA(SS_TAP(X_F2))); } // tty 2
     SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_3) { SS(SS_CA(SS_TAP(X_F3))); } // tty 3
     SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_4) { SS(SS_CA(SS_TAP(X_F4))); } // tty 4
     SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_5) { SS(SS_CA(SS_TAP(X_F5))); } // tty 5
