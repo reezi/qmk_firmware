08308b7c6f23b866efbed1104ad8816484ebb33b
reezi <reezi@gmail.com>
2023-02-23 18:35:12 +0100

    leader: change tty from alpha layer

--- keyboards/ferris/keymaps/my/leader.c
+++ keyboards/ferris/keymaps/my/leader.c
@@ -9,25 +9,25 @@ void matrix_scan_user(void) {
     SEQ_ONE_KEY(FR_F) { SS(SS_CG("f")); } // free
     SEQ_ONE_KEY(FR_T) { SS(SS_CG("t")); } // terminal
     SEQ_ONE_KEY(FR_W) { SS(SS_CG("w")); } // web
-    SEQ_ONE_KEY(FR_D) { SS(SS_CG("d")); } // voice (Discord)
-    SEQ_ONE_KEY(FR_A) { SS(SS_CG("a")); } // game (Adventure)
+    SEQ_ONE_KEY(FR_D) { SS(SS_CG("d")); } // voice -- d as in discord
+    SEQ_ONE_KEY(FR_A) { SS(SS_CG("a")); } // game -- a as in adventure
+    SEQ_ONE_KEY(FR_M) { SS(SS_CG("m")); } // menu
 
     SEQ_TWO_KEYS(FR_T, FR_H) { SS(SS_CA(SS_TAP(X_F1))); } // tty 1
     SEQ_TWO_KEYS(FR_T, FR_S) { SS(SS_CA(SS_TAP(X_F2))); } // tty 2
     SEQ_TWO_KEYS(FR_T, FR_T) { SS(SS_CG( SS_DOWN(X_T) SS_DOWN(X_N) SS_UP(X_N) SS_UP(X_T) )); } // new terminal
-    SEQ_TWO_KEYS(FR_D, FR_D) { SS(SS_CG( SS_DOWN(X_D) SS_DOWN(X_N) SS_UP(X_N) SS_UP(X_D) )); } // new voice
 
     SEQ_THREE_KEYS(FR_W, FR_O, FR_W) { layer_move(_WST); } // wow layer
     SEQ_THREE_KEYS(FR_S, FR_S, FR_R) { SS(SS_CG( SS_DOWN(X_S) SS_DOWN(X_R) SS_UP(X_R) SS_UP(X_S) )); } // sway reload
     SEQ_THREE_KEYS(FR_S, FR_S, FR_K) { SS(SS_CG( SS_DOWN(X_S) SS_DOWN(X_K) SS_UP(X_K) SS_UP(X_S) )); } // sway kill
 
-    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_1) { SS(SS_CA(SS_TAP(X_F1))); } // tty 1
-    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_2) { SS(SS_CA(SS_TAP(X_F2))); } // tty 2
-    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_3) { SS(SS_CA(SS_TAP(X_F3))); } // tty 3
-    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_4) { SS(SS_CA(SS_TAP(X_F4))); } // tty 4
-    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_5) { SS(SS_CA(SS_TAP(X_F5))); } // tty 5
-    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_6) { SS(SS_CA(SS_TAP(X_F6))); } // tty 6
-    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_7) { SS(SS_CA(SS_TAP(X_F7))); } // tty 7
+    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_B) { SS(SS_CA(SS_TAP(X_F1))); } // tty 1
+    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_Y) { SS(SS_CA(SS_TAP(X_F2))); } // tty 2
+    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_O) { SS(SS_CA(SS_TAP(X_F3))); } // tty 3
+    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_U) { SS(SS_CA(SS_TAP(X_F4))); } // tty 4
+    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_L) { SS(SS_CA(SS_TAP(X_F5))); } // tty 5
+    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_D) { SS(SS_CA(SS_TAP(X_F6))); } // tty 6
+    SEQ_FOUR_KEYS(FR_T, FR_T, FR_Y, FR_W) { SS(SS_CA(SS_TAP(X_F7))); } // tty 7
 
   }
 }
